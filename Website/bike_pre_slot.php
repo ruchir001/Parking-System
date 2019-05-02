a92503913c5f3fa0b198d53c83eb33ee52dfde49
Ruchir Bhaskar <ruchirbhaskar@gmail.com>
2019-05-02 13:30:25 +0530

    Add files via upload

---
+++ Website/bike_pre_slot.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+if(!isset($_SESSION['username']))
+{
+	echo "Not a session";
+}
+else
+{
+	$tmp_username = $_SESSION['username'];
+	echo $tmp_username;
+	echo "<br />";
+	$tmp_building = $_SESSION['building'];
+	echo $tmp_building;
+	echo "<br />";
+	$day=$_SESSION['day'];
+	echo $day;
+
+	include("bike_slots.php");
+}
+?>
