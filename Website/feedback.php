24fb55a1c3e6b3a141612470d8cf9f5be633984a
Ruchir Bhaskar <ruchirbhaskar@gmail.com>
2019-05-17 12:14:25 +0530

    Add files via upload

---
+++ Website/feedback.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+</head>
+<body background="background.jpeg" >
+<title>
+Feedback</title>
+</head>
+<body>
+<br><br><br><br><br><br><br><br><br><br><br><br><center>
+<form method="post" action="mail_test.php">
+
+<table>
+<tbody><tr><td width="10">Subject:</td><td><textarea name="subject" cols="30" rows="1"></textarea></td></tr>
+<tr><td width="10">Feedback:</td><td><textarea name="feedback" cols="30" rows="3"></textarea></td></tr>
+</tbody></table>
+<p align="center"><input type="submit" name="submit" value="Post Feedback">
+</p><p></p></form>
+</center>
+</body>
+</html>
